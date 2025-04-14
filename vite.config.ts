97c85a2e848551b29128cb5ee1bd1b2bfdc086d3
abadiapedro <abadiapedro98@gmail.com>
2025-04-14 08:36:34 -0300

    Fix - base path for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/portfolio-pedroguilherme/", // 👈 ADICIONE ESSA LINHA
+  base: "/",
   plugins: [react()],
 })
